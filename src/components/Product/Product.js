05d6dd7af2d851cc602f74b5532d5e3c112ca517
SheikhULAB <tosibalaptop86@gmail.com>
2020-10-11 06:42:28 +0600

    basic routing added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Product.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    const {img, name, seller, price, stock} = props.product;
+    const {img, name, seller, price, stock, key} = props.product;
     return (
         <div className="product">
             <div>
@@ -12,7 +13,7 @@ const Product = (props) => {
             </div>
 
             <div className="product-name">
-               <h4>{name}</h4>
+               <h4 className="product-name"><Link to={"/product/"+key}>{name}</Link></h4>
                <br/>
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
